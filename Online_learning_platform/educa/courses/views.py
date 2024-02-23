2c9589300a38dc22576aa0257f3f91061f512f34
Gnatolog <robertadamyanpy@gmail.com>
2024-02-23 23:12:19 +0300

    compleated add_cms

--- Online_learning_platform/educa/courses/views.py
+++ Online_learning_platform/educa/courses/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView # класс для получения списков
+from django.shortcuts import redirect, get_object_or_404  # если нет страницы то генерируем 404
+from django.views.generic.list import ListView  # класс для получения списков
 from .models import Course  # импортируем класс для которого будем
-                            # реализовывать представление
+# реализовывать представление
 from django.views.generic.edit import (
     CreateView,  # класс для создания представления
     UpdateView,  # класс для обновления представления
@@ -11,8 +11,16 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,  # миксин для логин
     PermissionRequiredMixin  # предостовляет пользоватею доступ
-                             # представлению с конкретным разрешением
+    # представлению с конкретным разрешением
 )
+from django.views.generic.base import TemplateResponseMixin, View  # миксины для формы
+from .forms import ModuleFormSet  # импортирем класс формы
+
+from django.forms.models import modelform_factory  # готовый класс джанго для загрузки данных
+from django.apps import apps  # получает фактичский класс для конкретного имени модели
+from .models import Module, Content  # импортируем классы которые подвязаны на загрузке даных
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin # импортируем миксины из module django-braces
+
 
 class ManageCourseListView(ListView):
     model = Course
@@ -47,19 +55,169 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
     template_name = 'courses/manage/course/form.html'
 
 
-
 class ManageCourseListView(OwnerCourseMixin, ListView):
     """
     Класс который отвечает определенному функционалу при представление
     """
-    template_name = 'courses/manage/course/list.html' # создаём url для данного функционала
+    template_name = 'courses/manage/course/list.html'  # создаём url для данного функционала
     permission_required = 'courses.view_course'
+
+
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
     permission_required = 'courses.add_course'
+
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
     permission_required = 'courses.change_course'
 
+
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    """
+    Класс обновления модулей и курсов
+    """
+    template_name = 'courses/manage/module/formset.html'  # url address
+    course = None  # default value
+
+    def get_formset(self, data=None):  # избегаем повторения исходного кода
+        return ModuleFormSet(instance=self.course,
+                             data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        owner=request.user)
+        return super().dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset})
+
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    def get_model(self, model_name):
+        if model_name in ['text', 'video', 'image', 'file']:
+            return apps.get_model(app_label='courses',
+                                  model_name=model_name)
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner',
+                                                 'order',
+                                                 'created',
+                                                 'updated'])
+        return Form(*args, **kwargs)
+
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(Module,
+                                       id=module_id,
+                                       course__owner=request.user)
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(self.model,
+                                         id=id,
+                                         owner=request.user)
+        return super().dispatch(request, module_id, model_name, id)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form,
+                                        'object': self.obj})
+
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model,
+                             instance=self.obj,
+                             data=request.POST,
+                             files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                # new content
+                Content.objects.create(module=self.module,
+                                       item=obj)
+            return redirect('module_content_list', self.module.id)
+        return self.render_to_response({'form': form,
+                                        'object': self.obj})
+
+class ContentDeleteView(View):
+    """
+    Класс для удаления контента
+    """
+    def post(self, request, id):
+        content = get_object_or_404(Content,
+                                    id=id,
+                                    module__course__owner=request.user)
+        module = content.module
+        content.item.delete()
+        content.delete()
+        return redirect('module_content_list', module.id)
+
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html' # url address
+
+    def get(self, request, module_id):
+        module = get_object_or_404(Module,
+                                   id=module_id,
+                                   course__owner=request.user)
+        return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(
+                      CsrfExemptMixin,
+                      JsonRequestResponseMixin,
+                      View
+):
+    """
+    Класс представления для смены мест модулей
+    """
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                   course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin,
+                       JsonRequestResponseMixin,
+                       View):
+    """
+    Класс представления для работы с перетаскивания контента внутри модуля
+    """
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                       module__course__owner=request.user) \
+                       .update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+
+
+
+
+
+
+
